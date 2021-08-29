dda173bb0133a52d064b0b8089fdcf01efaca7bd
ArnaudBru <brugiere.arnaud@gmail.com>
2021-08-30 01:18:30 +0200

    Update train.py
    Add training functions

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import pytorch_lightning as pl
-import torch
+from torch import nn
 
-class LitModel(pl.LightningModule):
+
+class SiameseNetwork(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
@@ -10,26 +11,22 @@ class LitModel(pl.LightningModule):
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(64),
             nn.MaxPool2d(2, stride=2),
-
             nn.Conv2d(64, 128, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(2, stride=2),
-
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(256),
             nn.MaxPool2d(2, stride=2),
-
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512)
-
-            nn.Flatten()
-
+            nn.BatchNorm2d(512),
+            nn.Flatten(),
             nn.Linear(256 * 512, 1024),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(1024))
+            nn.BatchNorm2d(1024),
+        )
 
     def forward_one_network(self, x):
         return self.net(x)
@@ -40,7 +37,27 @@ class LitModel(pl.LightningModule):
         return output1, output2
 
     def training_step(self, batch, batch_idx):
-        pass
+        x0, x1, y = batch
+        output1, output2 = self(x0, x1)
+        loss = self.criterion(output1, output2, y)
+        self.log("train_loss", loss, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x0, x1, y = batch
+        output1, output2 = self(x0, x1)
+        loss = self.criterion(output1, output2, y)
+
+        self.log("val_loss", loss, prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x0, x1, y = batch
+        output1, output2 = self(x0, x1)
+        loss = self.criterion(output1, output2, y)
+
+        self.log("test_loss", loss)
+        return loss
 
     def configure_optimizers(self):
         pass
