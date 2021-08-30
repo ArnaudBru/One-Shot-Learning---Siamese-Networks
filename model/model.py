d4d4fe95202225f4255015b62581a278f3f3e78a
ArnaudBru <brugiere.arnaud@gmail.com>
2021-08-31 01:26:19 +0200

    Update model.py
    Add FullyConnectedBlock class
    rename unclear variables

--- model/model.py
+++ model/model.py
@@ -17,7 +17,10 @@ class ConvolutionBlock(nn.Module):
         if max_pooling:
             self.conv = nn.Sequential(
                 nn.Conv2d(
-                    in_channels, out_channels, kernel_size=kernel_size, padding=padding
+                    in_channels,
+                    out_channels,
+                    kernel_size=kernel_size,
+                    padding=padding
                 ),
                 nn.ReLU(inplace=True),
                 nn.BatchNorm2d(out_channels),
@@ -29,7 +32,7 @@ class ConvolutionBlock(nn.Module):
                     in_channels,
                     out_channels,
                     kernel_size=kernel_size,
-                    padding=kernel_size,
+                    padding=padding,
                 ),
                 nn.ReLU(inplace=True),
                 nn.BatchNorm2d(out_channels),
@@ -39,59 +42,96 @@ class ConvolutionBlock(nn.Module):
         x = self.conv(x)
         return x
 
+class FullyConnectedBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        flatten: Optional[bool] = False,
+    ) -> None:
+        super(FullyConnectedBlock, self).__init__()
+        if flatten:
+            self.fc = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(in_channels, out_channels),
+                nn.ReLU(inplace=True),
+                nn.BatchNorm1d(out_channels)
+            )
+        else:
+            self.fc = nn.Sequential(
+                nn.Linear(in_channels, out_channels),
+                nn.ReLU(inplace=True),
+                nn.BatchNorm1d(out_channels),
+            )
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
 
 class SiameseNetwork(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
         self.learning_rate = 1e-4
+        # self.criterion = ...
 
         channels = 64
 
-        self.conv_block_1 = ConvolutionBlock(3, channels, kernel_size=5, padding=2, max_pooling=True)
-        self.conv_block_2 = ConvolutionBlock(channels, 2*channels, kernel_size=5, padding=2, max_pooling=True)
-        self.conv_block_3 = ConvolutionBlock(2*channels, 4*channels, kernel_size=3, padding=1, max_pooling=True)
-        self.conv_block_4 = ConvolutionBlock(4*channels, 8*channels, kernel_size=3, padding=1, max_pooling=False)
-
-        self.fc_block = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(4*channels * 8*channels, 16*channels),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(16*channels),
+        self.conv_block_1 = ConvolutionBlock(
+            3, channels, kernel_size=5, padding=2, max_pooling=True
+        )
+        self.conv_block_2 = ConvolutionBlock(
+            channels, 2 * channels, kernel_size=5, padding=2, max_pooling=True
+        )
+        self.conv_block_3 = ConvolutionBlock(
+            2 * channels, 4 * channels, kernel_size=3, padding=1, max_pooling=True
+        )
+        self.conv_block_4 = ConvolutionBlock(
+            4 * channels, 8 * channels, kernel_size=3, padding=1, max_pooling=False
         )
 
+        self.fc_block = FullyConnectedBlock(492032, 8, flatten=True)
+
     def _forward_one_network(self, x):
+        print('-----------------------')
+        # print(x.shape)
         x = self.conv_block_1(x)
+        # print(x.shape)
         x = self.conv_block_2(x)
+        # print(x.shape)
         x = self.conv_block_3(x)
+        # print(x.shape)
         x = self.conv_block_4(x)
+        # print(x.shape)
         x = self.fc_block(x)
+        # print(x.shape)
         return x
 
-    def forward(self, x1, x2):
-        output1 = self._forward_one_network(x1)
-        output2 = self._forward_one_network(x2)
-        return output1, output2
+    def forward(self, ref_images, other_images):
+        ref_output = self._forward_one_network(ref_images)
+        other_output = self._forward_one_network(other_images)
+        return ref_output, other_output
 
     def training_step(self, batch, batch_idx):
-        x0, x1, y = batch
-        output1, output2 = self(x0, x1)
-        loss = self.criterion(output1, output2, y)
+        ref_images, other_images, labels = batch
+        ref_output, other_output = self(ref_images, other_images)
+        loss = self.criterion(ref_output, other_output, labels)
         self.log("train_loss", loss, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x0, x1, y = batch
-        output1, output2 = self(x0, x1)
-        loss = self.criterion(output1, output2, y)
+        ref_images, other_images, labels = batch
+        ref_output, other_output = self(ref_images, other_images)
+        loss = self.criterion(ref_output, other_output, labels)
 
         self.log("val_loss", loss, prog_bar=True)
         return loss
 
     def test_step(self, batch, batch_idx):
-        x0, x1, y = batch
-        output1, output2 = self(x0, x1)
-        loss = self.criterion(output1, output2, y)
+        ref_images, other_images, labels = batch
+        ref_output, other_output = self(ref_images, other_images)
+        loss = self.criterion(ref_output, other_output, labels)
 
         self.log("test_loss", loss)
         return loss
@@ -99,10 +139,3 @@ class SiameseNetwork(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
-
-def main():
-    pass
-
-if __name__=="__main__":
-    main()
-
