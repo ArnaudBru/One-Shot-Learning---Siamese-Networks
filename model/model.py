fb5084717ab051883c82624956818fb5b1e89ade
ArnaudBru <brugiere.arnaud@gmail.com>
2021-09-02 01:30:01 +0200

    Update model.py
    Add Loss + clean code + Doc

--- model/model.py
+++ model/model.py
@@ -1,9 +1,10 @@
-from typing import Optional
+"""Model and Loss definition"""
+from typing import Optional, Tuple
 
 import pytorch_lightning as pl
-from torch import nn, optim
 import torch
 import torch.nn.functional as F
+from torch import nn, optim, Tensor
 
 
 class ContrastiveLoss(torch.nn.Module):
@@ -12,17 +13,38 @@ class ContrastiveLoss(torch.nn.Module):
     Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
     """
 
-    def __init__(self, margin):
+    def __init__(self, margin: float) -> None:
         super().__init__()
         self.margin = margin
 
-    def forward(self, output1, output2, label):
-        euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) + (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+    def forward(self, output1: Tensor, output2: Tensor, label: Tensor) -> float:
+        """Calculates Contrastive loss
+
+        Args:
+            output1 (Tensor):
+            output2 (Tensor):
+            label (Tensor):
+
+        Returns:
+            float
+        """
+        euclidean_distance = F.pairwise_distance(output1, output2, keepdim=True)
+        loss_contrastive = torch.mean(
+            (1 - label) * torch.pow(euclidean_distance, 2)
+            + (label)
+            * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
+        )
 
         return loss_contrastive
 
+
 class ConvolutionBlock(nn.Module):
+    """Convolution Block with ReLU activation, BatchNorm and Max-pooling (Optional)
+
+    Attributes:
+        conv (TYPE):
+    """
+
     def __init__(
         self,
         in_channels: int,
@@ -31,14 +53,11 @@ class ConvolutionBlock(nn.Module):
         padding: int,
         max_pooling: Optional[bool] = True,
     ) -> None:
-        super(ConvolutionBlock, self).__init__()
+        super().__init__()
         if max_pooling:
             self.conv = nn.Sequential(
                 nn.Conv2d(
-                    in_channels,
-                    out_channels,
-                    kernel_size=kernel_size,
-                    padding=padding
+                    in_channels, out_channels, kernel_size=kernel_size, padding=padding
                 ),
                 nn.ReLU(inplace=True),
                 nn.BatchNorm2d(out_channels),
@@ -56,42 +75,80 @@ class ConvolutionBlock(nn.Module):
                 nn.BatchNorm2d(out_channels),
             )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
+        """Computes output of the block
+
+        Args:
+            x (Tensor):
+
+        Returns:
+            Tensor
+        """
         x = self.conv(x)
         return x
 
+
 class FullyConnectedBlock(nn.Module):
+    """Fully connected block with ReLU activation and BatchNorm
+    Flattening can be added prior to the block (Optional)
+
+    Attributes:
+        fully_connected (TYPE):
+    """
+
     def __init__(
         self,
         in_channels: int,
         out_channels: int,
         flatten: Optional[bool] = False,
     ) -> None:
-        super(FullyConnectedBlock, self).__init__()
+        super().__init__()
         if flatten:
-            self.fc = nn.Sequential(
+            self.fully_connected = nn.Sequential(
                 nn.Flatten(),
                 nn.Linear(in_channels, out_channels),
                 nn.ReLU(inplace=True),
-                nn.BatchNorm1d(out_channels)
+                nn.BatchNorm1d(out_channels),
             )
         else:
-            self.fc = nn.Sequential(
+            self.fully_connected = nn.Sequential(
                 nn.Linear(in_channels, out_channels),
                 nn.ReLU(inplace=True),
                 nn.BatchNorm1d(out_channels),
             )
 
-    def forward(self, x):
-        x = self.fc(x)
+    def forward(self, x: Tensor) -> Tensor:
+        """Computes output of the block
+
+        Args:
+            x (Tensor):
+
+        Returns:
+            Tensor
+        """
+        x = self.fully_connected(x)
         return x
 
 
 class SiameseNetwork(pl.LightningModule):
-    def __init__(self):
+
+    """
+
+    Attributes:
+        conv_block_1 (TYPE): Convolutional block 1
+        conv_block_2 (TYPE): Convolutional block 2
+        conv_block_3 (TYPE): Convolutional block 3
+        conv_block_4 (TYPE): Convolutional block 4
+        criterion (TYPE): Loss function
+        fc_block (TYPE): Fully Connected block
+        learning_rate (float): learning rate of the optimizer
+    """
+
+    def __init__(self) -> None:
         super().__init__()
 
         self.learning_rate = 1e-4
+        # TODO : margin as parameter
         self.criterion = ContrastiveLoss(margin=1.0)
 
         channels = 64
@@ -111,42 +168,37 @@ class SiameseNetwork(pl.LightningModule):
 
         self.fc_block = FullyConnectedBlock(492032, 8, flatten=True)
 
-    def _forward_one_network(self, x):
-        # print('-----------------------')
-        # print(x.shape)
+    def _forward_one_network(self, x: Tensor) -> Tensor:
         x = self.conv_block_1(x)
-        # print(x.shape)
         x = self.conv_block_2(x)
-        # print(x.shape)
         x = self.conv_block_3(x)
-        # print(x.shape)
         x = self.conv_block_4(x)
-        # print(x.shape)
         x = self.fc_block(x)
-        # print(x.shape)
         return x
 
-    def forward(self, ref_images, other_images):
+    def forward(
+        self, ref_images: Tensor, other_images: Tensor
+    ) -> Tuple[Tensor, Tensor]:
         ref_output = self._forward_one_network(ref_images)
         other_output = self._forward_one_network(other_images)
         return ref_output, other_output
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx) -> float:
         ref_images, other_images, labels = batch
         ref_output, other_output = self(ref_images, other_images)
         loss = self.criterion(ref_output, other_output, labels)
         self.log("train_loss", loss, on_epoch=True)
         return loss
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx) -> float:
         ref_images, other_images, labels = batch
         ref_output, other_output = self(ref_images, other_images)
         loss = self.criterion(ref_output, other_output, labels)
 
-        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True)
         return loss
 
-    def test_step(self, batch, batch_idx):
+    def test_step(self, batch, batch_idx) -> float:
         ref_images, other_images, labels = batch
         ref_output, other_output = self(ref_images, other_images)
         loss = self.criterion(ref_output, other_output, labels)
@@ -154,6 +206,6 @@ class SiameseNetwork(pl.LightningModule):
         self.log("test_loss", loss)
         return loss
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> optim.Optimizer:
         optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
